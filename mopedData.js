54cee6c355e8320b177954d32abc711ca68ed6b2
Brice Sinkule <bsinkule@gmail.com>
2018-09-07 10:47:28 -0600

    initial commit

---
+++ mopedData.js
@@ -0,0 +1,26 @@
+module.exports = [
+{
+    owner: "Brice",
+    brand: "Peugeot",
+    year: 1977,
+    rating: 9
+},
+{
+    owner: "Allen",
+    brand: "Honda",
+    year: 1983,
+    rating: 6
+},
+{
+    owner: "Steve",
+    brand: "Tomos",
+    year: 2009,
+    rating: 7
+},
+{
+    owner: "Connor",
+    brand: "Motobecane",
+    year: 1979,
+    rating: 10
+}
+]
