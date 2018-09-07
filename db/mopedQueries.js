067e8d3906569a487c3aaf43f457e98864b3eb2a
Brice Sinkule <bsinkule@gmail.com>
2018-09-07 12:39:18 -0600

    took out a comment

--- db/mopedQueries.js
+++ db/mopedQueries.js
@@ -1,9 +1,7 @@
 const knex = require('./connection')
 
-const getAll = () => knex('mopeds') // 'ipas' is the [table_name]
+const getAll = () => knex('mopeds') 
 
-// const getOneBeer = () =>  knex('ipas').where('id', req.params.id)
-// ABOVE won't work cause we don't have access to req.params.id anymore, BELOW works
 const getOne = (id) =>  knex('mopeds').where('id', id)
 
 const postOne = (body) => knex('mopeds').insert(body).returning('*')
